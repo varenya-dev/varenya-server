0f100c46874a77d44708b172ce1c0c6a8375ad22
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-10-18 11:09:03 +0530

    Chat Notification DTO

--- server/src/modules/notifications/notification.controller.ts
+++ server/src/modules/notifications/notification.controller.ts
@@ -4,12 +4,12 @@ import { FirebaseAuthGuard } from '../../guards/firebase-auth.guard';
 import { NotificationService } from './notification.service';
 import { Controller, UseGuards, Post, Body } from '@nestjs/common';
 
-@Controller('chat')
+@Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
   @UseGuards(FirebaseAuthGuard)
-  @Post('notification')
+  @Post('chat')
   public async sendNotificationToDevices(
     @AuthUser() firebaseUser: auth.UserRecord,
     @Body('threadId') threadId: string,
