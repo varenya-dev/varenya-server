9746aebdeeeb3d9052d164188cbb3939bd5e8fd5
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-11-22 09:35:16 +0530

    Roles set for User

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,16 +1,19 @@
+import { RoleAuthGuard } from 'src/guards/role-auth.guard';
 import { UserService } from './user.service';
 import { Controller, Delete, UseGuards } from '@nestjs/common';
-import { FirebaseAuthGuard } from 'src/guards/firebase-auth.guard';
 import { AuthUser } from 'src/decorators/auth-user.decorator';
 import { auth } from 'firebase-admin';
 import { User } from 'src/models/user.model';
+import { Role } from 'src/decorators/role.decorator';
+import { Roles } from 'src/enum/roles.enum';
 
 @Controller('user')
+@UseGuards(RoleAuthGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(FirebaseAuthGuard)
   @Delete()
+  @Role(Roles.Main, Roles.Professional)
   public async deleteUser(
     @AuthUser() firebaseUser: auth.UserRecord,
   ): Promise<User> {
