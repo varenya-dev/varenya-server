0990d282a47d65985f1da7ab2e391170970e1818
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-11-22 09:33:21 +0530

    RolesAuthGuard implemented for Notifications

--- server/src/modules/notifications/notification.controller.ts
+++ server/src/modules/notifications/notification.controller.ts
@@ -1,17 +1,20 @@
+import { RoleAuthGuard } from 'src/guards/role-auth.guard';
 import { ResponseNotificationDto } from './../../dto/notification/response-notification.dto';
 import { ChatNotificationDto } from './../../dto/notification/chat-notification.dto';
 import { auth } from 'firebase-admin';
 import { AuthUser } from '../../decorators/auth-user.decorator';
-import { FirebaseAuthGuard } from '../../guards/firebase-auth.guard';
 import { NotificationService } from './notification.service';
 import { Controller, UseGuards, Post, Body } from '@nestjs/common';
+import { Role } from 'src/decorators/role.decorator';
+import { Roles } from 'src/enum/roles.enum';
 
+@UseGuards(RoleAuthGuard)
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
-  @UseGuards(FirebaseAuthGuard)
   @Post('chat')
+  @Role(Roles.Main, Roles.Professional)
   public async handleChatNotifications(
     @AuthUser() firebaseUser: auth.UserRecord,
     @Body() chatNotificationDto: ChatNotificationDto,
@@ -22,16 +25,16 @@ export class NotificationController {
     );
   }
 
-  @UseGuards(FirebaseAuthGuard)
   @Post('sos')
+  @Role(Roles.Main)
   public async handleSOSNotifications(
     @AuthUser() firebaseUser: auth.UserRecord,
   ): Promise<void> {
     return await this.notificationService.handleSOSNotifications(firebaseUser);
   }
 
-  @UseGuards(FirebaseAuthGuard)
   @Post('sos/response')
+  @Role(Roles.Main, Roles.Professional)
   public async handleSOSResponseNotifications(
     @AuthUser() firebaseUser: auth.UserRecord,
     @Body() responseNotificationDto: ResponseNotificationDto,
