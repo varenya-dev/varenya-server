a257cb467c9d766e1d7604eeb2b9ef4dd245dc3f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-12-21 13:19:43 +0530

    First DTO Implementation for creation of posts. Working on #68

---
+++ server/src/dto/post/new-post.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsUrl,
+  ValidateNested,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  public body: string;
+
+  @IsArray()
+  @ArrayMinSize(0)
+  @ArrayMaxSize(4)
+  @IsUrl({}, { each: true })
+  public images: string[];
+
+  @IsArray()
+  @ArrayMinSize(1)
+  public categories: string[];
+}
