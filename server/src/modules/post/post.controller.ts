1ac38cffd28df12666ccc8c0cb2430832570581d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-12-21 16:59:40 +0530

    Controller Implementation for deleting posts. Closes #73

--- server/src/modules/post/post.controller.ts
+++ server/src/modules/post/post.controller.ts
@@ -1,13 +1,14 @@
 import { UpdatePostDto } from './../../dto/post/update-post.dto';
 import { PostService } from './post.service';
 import { RoleAuthGuard } from 'src/guards/role-auth.guard';
-import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Put, UseGuards } from '@nestjs/common';
 import { Role } from 'src/decorators/role.decorator';
 import { Roles } from 'src/enum/roles.enum';
 import { CreatePostDto } from 'src/dto/post/new-post.dto';
 import { Post as PostModel } from 'src/models/post.model';
 import { AuthUser } from 'src/decorators/auth-user.decorator';
 import { LoggedInUser } from 'src/dto/logged-in-user.dto';
+import { DeletePostDto } from 'src/dto/post/delete-post.dto';
 
 @Controller('post')
 @UseGuards(RoleAuthGuard)
@@ -30,5 +31,14 @@ export class PostController {
     @Body() updatePostDto: UpdatePostDto,
   ): Promise<PostModel> {
     return await this.postService.updatePost(loggedInUser, updatePostDto);
+  }
+
+  @Delete()
+  @Role(Roles.Main, Roles.Professional)
+  public async deletePost(
+    @AuthUser() loggedInUser: LoggedInUser,
+    @Body() deletePostDto: DeletePostDto,
+  ): Promise<PostModel> {
+    return await this.postService.deletePost(loggedInUser, deletePostDto);
   }
 }
