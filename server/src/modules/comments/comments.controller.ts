7e9d2caae343fe10b915a8a35e0ed578c7a0f01f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-03 08:59:31 +0530

    First Controller Implementation for deleting comment. Working on #96

--- server/src/modules/comments/comments.controller.ts
+++ server/src/modules/comments/comments.controller.ts
@@ -2,12 +2,13 @@ import { UpdateCommentDto } from './../../dto/comments/update-comment.dto';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './../../dto/comments/create-comment.dto';
 import { RoleAuthGuard } from './../../guards/role-auth.guard';
-import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Put, UseGuards } from '@nestjs/common';
 import { Role } from 'src/decorators/role.decorator';
 import { Roles } from 'src/enum/roles.enum';
 import { AuthUser } from 'src/decorators/auth-user.decorator';
 import { LoggedInUser } from 'src/dto/logged-in-user.dto';
 import { Post as PostModel } from 'src/models/post.model';
+import { DeleteCommentDto } from 'src/dto/comments/delete-comment.dto';
 
 @Controller('comment')
 @UseGuards(RoleAuthGuard)
@@ -35,6 +36,19 @@ export class CommentsController {
     return await this.commentsService.updateComment(
       loggedInUser,
       updateCommentDto,
+    );
+  }
+
+  @Delete()
+  @Role(Roles.Main, Roles.Professional)
+  public async deleteComment(
+    @AuthUser() loggedInUser: LoggedInUser,
+    @Body() deleteCommentDto: DeleteCommentDto,
+  ): Promise<PostModel> {
+    // NEED TO TEST.
+    return await this.commentsService.deleteComment(
+      loggedInUser,
+      deleteCommentDto,
     );
   }
 }
