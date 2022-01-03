fb276fe53d625149a0f5e77318b4c7b1f3912f48
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-03 08:30:28 +0530

    First Service Implementation for creating comments. Working on #89

--- server/src/modules/comments/comments.service.ts
+++ server/src/modules/comments/comments.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { CreateCommentDto } from './../../dto/comments/create-comment.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { LoggedInUser } from 'src/dto/logged-in-user.dto';
 import { Post } from 'src/models/post.model';
 import { Repository } from 'typeorm';
+import { PostType } from 'src/enum/post-type.enum';
 
 @Injectable()
 export class CommentsService {
@@ -9,4 +12,26 @@ export class CommentsService {
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
   ) {}
+
+  public async createComment(
+    loggedInUser: LoggedInUser,
+    createCommentDto: CreateCommentDto,
+  ): Promise<Post> {
+    // NEED TO TEST.
+    const checkPost = await this.postRepository.findOne({
+      id: createCommentDto.postId,
+    });
+
+    if (!checkPost) {
+      throw new NotFoundException('Post could not be found.');
+    }
+
+    const newComment = new Post();
+    newComment.body = createCommentDto.comment;
+    newComment.user = loggedInUser.databaseUser;
+    newComment.postType = PostType.Comment;
+    newComment.post = checkPost;
+
+    return await this.postRepository.save(newComment);
+  }
 }
