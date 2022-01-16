263ec8f68aa145ffd9152e1591d9451189511e73
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-16 09:11:37 +0530

    Service Implementation for fetching activity. Closes #106

--- server/src/modules/activity/activity.service.ts
+++ server/src/modules/activity/activity.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { LoggedInUser } from 'src/dto/logged-in-user.dto';
 import { Activity } from 'src/models/activity.model';
 import { Appointment } from 'src/models/appointment.model';
 import { Post } from 'src/models/post.model';
@@ -12,6 +13,28 @@ export class ActivityService {
     @InjectRepository(Activity)
     private readonly activityRepository: Repository<Activity>,
   ) {}
+
+  public async fetchActivity(loggedInUser: LoggedInUser): Promise<Activity[]> {
+    return await this.activityRepository.find({
+      where: {
+        user: loggedInUser.databaseUser,
+      },
+      order: {
+        createdAt: 'DESC',
+      },
+      relations: [
+        'post',
+        'appointment',
+        'post.categories',
+        'post.images',
+        'post.user',
+        'post.comments',
+        'post.comments.user',
+        'appointment.doctorUser',
+        'appointment.patientUser',
+      ],
+    });
+  }
 
   public async recordPost(user: User, post: Post): Promise<Activity> {
     const newActivity = new Activity();
