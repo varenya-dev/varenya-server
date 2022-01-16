044d76ee55a828ad1d976ca3510ddbc3dae8bf1f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-16 09:11:51 +0530

    Controller Implementation for fetching activity. Closes #107

--- server/src/modules/activity/activity.controller.ts
+++ server/src/modules/activity/activity.controller.ts
@@ -1,9 +1,22 @@
 import { RoleAuthGuard } from './../../guards/role-auth.guard';
 import { ActivityService } from './activity.service';
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthUser } from 'src/decorators/auth-user.decorator';
+import { LoggedInUser } from 'src/dto/logged-in-user.dto';
+import { Activity } from 'src/models/activity.model';
+import { Role } from 'src/decorators/role.decorator';
+import { Roles } from 'src/enum/roles.enum';
 
 @Controller('activity')
 @UseGuards(RoleAuthGuard)
 export class ActivityController {
   constructor(private readonly activityService: ActivityService) {}
+
+  @Get()
+  @Role(Roles.Main)
+  public async fetchActivity(
+    @AuthUser() loggedInUser: LoggedInUser,
+  ): Promise<Activity[]> {
+    return await this.activityService.fetchActivity(loggedInUser);
+  }
 }
