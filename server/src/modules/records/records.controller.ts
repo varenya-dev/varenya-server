6f58f129c2e649f6ab14fdaf8db9ee0af553770d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-08 17:58:24 +0530

    Controller Implementation for fetching doctors for users. Closes #114

--- server/src/modules/records/records.controller.ts
+++ server/src/modules/records/records.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Doctor } from 'src/models/doctor.model';
+import { RoleAuthGuard } from './../../guards/role-auth.guard';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { RecordsService } from './records.service';
+import { Role } from 'src/decorators/role.decorator';
+import { Roles } from 'src/enum/roles.enum';
+import { AuthUser } from 'src/decorators/auth-user.decorator';
+import { LoggedInUser } from 'src/dto/logged-in-user.dto';
 
 @Controller('records')
-export class RecordsController {}
+@UseGuards(RoleAuthGuard)
+export class RecordsController {
+  constructor(private readonly recordsService: RecordsService) {}
+
+  @Get()
+  @Role(Roles.Main)
+  public async fetchRecords(
+    @AuthUser() loggedInUser: LoggedInUser,
+  ): Promise<Doctor[]> {
+    return await this.recordsService.fetchLinkedDoctors(loggedInUser);
+  }
+}
